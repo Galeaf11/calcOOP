318a58be6f8620c67ae8a13e7366b692c9e1a750
Galeaf11 <43822362+Galeaf11@users.noreply.github.com>
2018-11-20 11:17:29 +0600

    Update mainObjMethods.js

--- js/mainObjMethods.js
+++ js/mainObjMethods.js
@@ -1,27 +1,32 @@
 class Calculator{
-    plus(a, b) {
-        return a + b
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
     }
-    minus(a, b) {
-        return a - b
+    plus() {
+        return this.a + this.b
     }
-    inc(a, b) {
-        return a * b
+    minus() {
+        return this.a - this.b
     }
-    divide(a, b) {
-        if(b === 0)  return 'На ноль делить нельзя';
-        return a / b
+    inc() {
+        return this.a * this.b
     }
-    pow(a, b) {
-        return Math.pow(a, b)
+    divide() {
+        if(this.b === 0)  return 'На ноль делить нельзя';
+        return this.a / this.b
+    }
+    pow() {
+        return Math.pow(this.a, this.b)
     }
     info() {
-        console.log('Для вычисления суммы, введите команду calc.plus(a, b)');
-        console.log('Для вычисления разности, введите команду calc.minus(a, b)');
-        console.log('Для вычисления произведения, введите команду calc.inc(a, b)');
-        console.log('Для вычесления частного, введите команду calc.divide(a, b)');
-        console.log('Для того, чтобы возвести число в степень, введите команду calc.pow(a, b)');
+        console.log('Для вычисления суммы, введите команду calc.plus()');
+        console.log('Для вычисления разности, введите команду calc.minus()');
+        console.log('Для вычисления произведения, введите команду calc.inc()');
+        console.log('Для вычесления частного, введите команду calc.divide()');
+        console.log('Для того, чтобы возвести число в степень, введите команду calc.pow()');
     }
 }
-let calc = new Calculator();
-console.log("Для вычисления нужно воспользоваться методами объекта calc.(plus, minus, inc, divide, pow)(a, b), для более подробной информации введите calc.info()");
+let calc = new Calculator(1, 5);
+
+console.log("Для вычисления нужно воспользоваться методами объекта calc.(plus, minus, inc, divide, pow)(), для более подробной информации введите calc.info()");
